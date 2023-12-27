42d509d3ad5ca977343ff5aa848efd6dbae17fed
ojiwon99 <dhwldnjs0420@naver.com>
2023-12-27 15:33:22 +0900

    2023-12-27 자바

--- src/com/javaex/ex03/Ex11.java
+++ src/com/javaex/ex03/Ex11.java
@@ -9,6 +9,7 @@ public class Ex11 {
 		System.out.println("월을 입력하세요:");
 		int code = sc.nextInt();
 		
+		//char code =
 		switch (code) {
 		case 2: System.out.println("28일 입니다.");break;
 		case 4,6,9,11: System.out.println("30일 입니다.");break;	
@@ -17,15 +18,14 @@ public class Ex11 {
 		sc.close();
 	}      
 	   //default:System.out.println("31일 입니다.");break;만 해도 됨 나머지값
+	   //case 1,3,5,7,8,10,12: System.out.println("31일 입니다.");break
+	
 }       /*
           switch (month) {
              case 4:  
              case 6: 
              case 9: 
-             case 11: 
-             
-             case 1,3,5,7,8,10,12: System.out.println("31일 입니다.");break;
-             
+             case 11:
                System.out.println("30일 입니다.");
                break;
                 
